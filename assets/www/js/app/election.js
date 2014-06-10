021240c88b615e5fa76f7e6046f6ef1bc53dbb93
a <a@localhost>
2014-06-10 18:05:54 +0200

    grid

--- assets/www/js/app/election.js
+++ assets/www/js/app/election.js
@@ -1,27 +1,74 @@
 var election = {
+	initialize: function(){
+        $( document ).on( "pageshow", "#elezioniPage", function() {
+        	//navigator.notification.alert("Elezioni Page", function(){});
+        	election.list();
+        });
+	},
     save: function() {
-    	var onTxError = function (tx, err) {
-    		var msgText;
-    		if(err) {
-	    		//Tell the user what happened
-	    		msgText = "TX: " + err.message + " (" + err.code + ")";
+    	var tipologia = $('#elezione_tipologia').val();
+    	var data = $('#elezione_data').val();
+    	var note = $('#elezione_note').val();
+    	var query = 'INSERT INTO ELEZIONE(tipologia, data, note) VALUES("' + tipologia + '", ' + data + ', "' + note + '")';
+    	app.executeUpdate(query);
+    },
+    list: function(){
+    	var query = "SELECT * FROM ELEZIONE ORDER BY ID ASC";
+    	
+    	var onQuerySuccess = function (tx, results) {
+    		
+			if (results.rows) {
+				
+				console.log("Rows: " + results.rows);
+				var len = results.rows.length;
+				if (len > 0) {
+					var html = "<div class=\"ui-grid-b\">";
+					for ( var i = 0; i < len; i++) {
+						var data = new Date(results.rows.item(i).data);
+						var tipologia = results.rows.item(i).tipologia;
+						var note = results.rows.item(i).note;
+						console.log('Election: ' + tipologia + "-" + data + "-" + note);
+						html += "<div class=\"ui-block-a\"><div class=\"ui-bar ui-bar-a\" style=\"height:60px\">" + tipologia + "</div></div>";
+						html += "<div class=\"ui-block-b\"><div class=\"ui-bar ui-bar-a\" style=\"height:60px\">" + data + "</div></div>";
+						html += "<div class=\"ui-block-c\"><div class=\"ui-bar ui-bar-a\" style=\"height:60px\">" + note + "</div></div>";
+					}
+					html += "</div>"; 
+					// Use JQuery's $() to assign the content to the page
+					$("#elezioniList").html(html);
+					// Then open the View page to display the data
+					$.mobile.changePage("#elezioniPage", "slide", false, true);
+				} else {
+					navigator.notification.alert("No rows.");
+				}
+			} else {
+				navigator.notification.alert("No records match selection criteria.");
+			}
+		}
+    	var onSuccess = function (tx, res) {
+    		var html = "<div class=\"ui-grid-b\">";
+    		if(res) {
+	    		console.log("Insert ID: " + res.insertID);
+	    		console.log("Row affected: " + res.rowAffected);
+	    		console.log("Rows: " + res.rows.length);
+	    		if (res.rows) {
+	    			var len = res.rows.length;
+		    		if(len > 0) {
+			    		for(var i = 0; i < len; i++) {
+			    			console.log('Election: ' + res.rows[i]);
+//			    	        <div class="ui-block-a"><div class="ui-bar ui-bar-a" style="height:60px">Block A</div></div>
+//			    	        <div class="ui-block-b"><div class="ui-bar ui-bar-a" style="height:60px">Block B</div></div>
+//			    	        <div class="ui-block-c"><div class="ui-bar ui-bar-a" style="height:60px">Block C</div></div>
+			    		}
+		    		} else {
+		    			navigator.notification.alert("No records processed.");
+		    		}
+	    		}
     		} else {
-    			msgText = "TX: Unknown error";
+    			navigator.notification.alert("No results returned.");
     		}
-    		console.error(msgText);
-    		alert(msgText);
-    	};
-    	
-    	var onTxSuccess = function () {
-    		console.log("TX: success");
-    	};
-    	
-    	var createTable = function (tx){
-			var sqlStr = 'INSERT INTO ELEZIONE VALUES (id INTEGER primary key autoincrement, tipologia TEXT, data INT, note TEXT)';
-			console.log(sqlStr);
-			tx.executeSql(sqlStr, [], onTxSuccess, onTxError);
-    	};
-    
-    	app.db.transaction(createTable, onTxError, onTxSuccess);
+    		html += "</div>"; 
+		}
+		
+    	app.executeSelect(query, onQuerySuccess);
     }
 };
--- assets/www/js/app/init.js
+++ assets/www/js/app/init.js
@@ -16,10 +16,16 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-    	navigator.notification.alert("Device Ready!", function(){}, "App Ready", "Ko");
+    	
     },
     //Create database if it's not exist
-    initDatabase: function(){
+    initDatabase: function(){    
+    	this.db = window.openDatabase("slDB", "1.0", "Scrutinio Live", 3 * 1024 * 1024);//3mb
+    	
+    	var query = 'CREATE TABLE IF NOT EXISTS ELEZIONE (id INTEGER primary key autoincrement, tipologia TEXT, data INT, note TEXT)';
+    	this.executeUpdate(query);
+    },
+    executeUpdate: function(query){
     	
     	var onTxError = function (tx, err) {
     		var msgText;
@@ -37,13 +43,54 @@ var app = {
     		console.log("TX: success");
     	};
     	
-    	var createTable = function (tx){
-			var sqlStr = 'CREATE TABLE IF NOT EXISTS ELEZIONE (id INTEGER primary key autoincrement, tipologia TEXT, data INT, note TEXT)';
-			console.log(sqlStr);
-			tx.executeSql(sqlStr, [], onTxSuccess, onTxError);
+    	var updateQuery = function (tx){
+			console.log(query);
+			tx.executeSql(query, [], onTxSuccess, onTxError);
     	};
-    
-    	this.db = window.openDatabase("slDB", "1.0", "Scrutinio Live", 3 * 1024 * 1024);//3mb
-    	this.db.transaction(createTable, onTxError, onTxSuccess);
+    	
+    	this.db.transaction(updateQuery, onTxError, onTxSuccess);
+    },
+    executeSelect: function(query, customSuccess){
+    	var onSqlSuccess = function (tx, res) {
+    		console.log('SQL: success');
+    	};
+
+		var onSqlError = function (tx, err) {
+    		var msgText;
+    		if(err) {
+    			msgText = "SQL: " + err.message + " (" + err.code + ")";
+    		} else {
+    			msgText = "SQL: Unknown error";
+    		}
+    		console.error(msgText);
+    		alert(msgText);
+		}
+    	
+    	var onTxError = function (tx, err) {
+    		var msgText;
+    		if(err) {
+	    		//Tell the user what happened
+	    		msgText = "TX: " + err.message + " (" + err.code + ")";
+    		} else {
+    			msgText = "TX: Unknown error";
+    		}
+    		console.error(msgText);
+    		alert(msgText);
+    	};
+    	
+    	var onTxSuccess = function () {
+    		console.log("TX: success");
+    	};
+    	
+    	var updateQuery = function (tx){
+			console.log(query);
+			if(customSuccess){
+				//switch to custom success function
+				onSqlSuccess = customSuccess;
+			}
+			tx.executeSql(query, [], onSqlSuccess, onSqlError);
+    	};
+    	
+    	this.db.transaction(updateQuery, onTxError, onTxSuccess);
     }
 };
